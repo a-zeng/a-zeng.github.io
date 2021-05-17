b1272c3f44273a69a780aa8d638bbbcabd9003d8
Alex Zeng <azeng68@gmail.com>
2021-05-17 00:44:56 -0400

    First full version of the website
    Finished Rover and SeaPerch sections, as well as adding a project sorting system for the future

---
+++ js/sort-buttons.js
@@ -0,0 +1,18 @@
+function sortAll() {
+    $(".sort-all").fadeIn();
+}
+
+function sortRobotics() {
+    $(".sort-all").hide();
+    $(".sort-rob").fadeIn();
+}
+
+function sortCAD() {
+    $(".sort-all").hide();
+    $(".sort-cad").fadeIn();
+}
+
+function sortCoding() {
+    $(".sort-all").hide();
+    $(".sort-code").fadeIn();
+}
